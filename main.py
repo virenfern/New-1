98974da03e98705a704ec60c0fd69c0f91a397c0
virenfern <virenfern2006@gmail.com>
2024-10-16 13:40:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import random
-n=random.randint(0,100)
-a=-1
-guesses=0
-while(a!=n):
-    guesses+=1
-    a=int(input("enter your number:"))
-    if(a>n):
-        print("lower number please")
-    else:
-        print("higher number please")
-
-print(f"guesses correctly in {guesses}guesses")
+print("Hello World")
