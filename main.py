4b6d89c75f1078459840076d7e1405b88a6f149e
virenfern <virenfern2006@gmail.com>
2024-10-21 17:51:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello World")
+import random 
+print("Welcome to password storer!:")
+name=input("Enter your name user:").title()
+chat={
+    "Hello":["Hi there!","Hello"],
+    "hi":["hello","hi"]
+}
+
+def response():
+    user=input("").title()
+    for key in chat:
+        if (user==key):
+            print(random.choice(chat[key]))
+        else:
+            print("Invalid response:")
+
+def password():
+    print(f"What password do you want {name}:")
+    print("Enter the number!")
+    ch=int(input("1.computer,2.lab,3.pesuacademy"))
+    passwords=[ "041269",8861259602,"pukku123456"]
+    if(ch==1):
+       print(passwords[0])
+    elif(ch=="2"):
+       print(passwords[1])
+    elif(ch=="3"):
+        print(passwords[3])
+    else:
+        print("Invalid entry:")
+response()
+password()
