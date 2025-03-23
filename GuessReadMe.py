2d8a4fea5809d188d409503fa5ef52b208a58a10
virenfern <virenfern2006@gmail.com>
2025-03-23 10:35:59 +0300

    Create GuessReadMe.py

---
+++ GuessReadMe.py
@@ -0,0 +1,8 @@
+The Number Guessing Game is a simple Python program where the computer randomly selects a number between 0 and 100, and the player must guess it.
+The program provides hints (higher or lower) until the player finds the correct number.
+The game keeps track of the number of attempts.
+
+Random number generation between 0 and 100
+User-friendly hints for guiding the player
+Tracks number of guesses
+Simple console-based interaction
